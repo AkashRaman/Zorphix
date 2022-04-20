d773f499f9b2fe8facc28abdeec033b387369109
AkashRaman <akashjayaraman8841@gmail.com>
2022-04-20 14:39:39 +0530

    Team & Event Modal added

--- js/mobile-script.js
+++ js/mobile-script.js
@@ -1,91 +1,91 @@
 $(document).ready(function() {
     var deviceWidth = $(window).width();
     var deviceHeight = $(window).height();
-    var silambattamHeight = $('#silambattam').height();
-    var businessEvent2 = $('#business-event-2').height();
+    var businessEvent1Height = $('#businessEvent1').height();
+    var businessEvent2 = $('#businessEvent2').height();
     if ( deviceWidth <= "767") {
-        $('#summerCamp').waypoint(function(direction) {
+        $('#technicalEvent1').waypoint(function(direction) {
             if( direction == "down") {
-                $('#summerCamp').addClass('mobileEffect'); 
+                $('#technicalEvent1').addClass('mobileEffect'); 
             } else {
-                $('#summerCamp').removeClass('mobileEffect'); 
+                $('#technicalEvent1').removeClass('mobileEffect'); 
             }
         }, {
             offset: "50%"
         });
-        $('#football').waypoint(function(direction) {
+        $('#technicalEvent2').waypoint(function(direction) {
             if( direction == "down") {
-                $('#football').addClass('mobileEffect');
-                $('#summerCamp').removeClass('mobileEffect'); 
+                $('#technicalEvent2').addClass('mobileEffect');
+                $('#technicalEvent1').removeClass('mobileEffect'); 
             } else {
-                $('#football').removeClass('mobileEffect'); 
-                $('#summerCamp').addClass('mobileEffect'); 
+                $('#technicalEvent2').removeClass('mobileEffect'); 
+                $('#technicalEvent1').addClass('mobileEffect'); 
             }
         }, {
             offset: "50%"
         });
-        $('#boxCricket').waypoint(function(direction) {
+        $('#technicalEvent3').waypoint(function(direction) {
             if( direction == "down") {
-                $('#boxCricket').addClass('mobileEffect');
-                $('#football').removeClass('mobileEffect'); 
+                $('#technicalEvent3').addClass('mobileEffect');
+                $('#technicalEvent2').removeClass('mobileEffect'); 
             } else {
-                $('#boxCricket').removeClass('mobileEffect'); 
-                $('#football').addClass('mobileEffect'); 
+                $('#technicalEvent3').removeClass('mobileEffect'); 
+                $('#technicalEvent2').addClass('mobileEffect'); 
             }
         }, {
             offset: "50%"
         });
-        $('#skating').waypoint(function(direction) {
+        $('#nonTechnicalEvent1').waypoint(function(direction) {
             if( direction == "down") {
-                $('#skating').addClass('mobileEffect');
-                $('#boxCricket').removeClass('mobileEffect'); 
+                $('#nonTechnicalEvent1').addClass('mobileEffect');
+                $('#technicalEvent3').removeClass('mobileEffect'); 
             } else {
-                $('#skating').removeClass('mobileEffect'); 
-                $('#boxCricket').addClass('mobileEffect'); 
+                $('#nonTechnicalEvent1').removeClass('mobileEffect'); 
+                $('#technicalEvent3').addClass('mobileEffect'); 
             }
         }, {
             offset: "50%"
         });
-        $('#crossfit').waypoint(function(direction) {
+        $('#nonTechnicalEvent2').waypoint(function(direction) {
             if( direction == "down") {
-                $('#crossfit').addClass('mobileEffect');
-                $('#skating').removeClass('mobileEffect'); 
+                $('#nonTechnicalEvent2').addClass('mobileEffect');
+                $('#nonTechnicalEvent1').removeClass('mobileEffect'); 
             } else {
-                $('#crossfit').removeClass('mobileEffect'); 
-                $('#skating').addClass('mobileEffect'); 
+                $('#nonTechnicalEvent2').removeClass('mobileEffect'); 
+                $('#nonTechnicalEvent1').addClass('mobileEffect'); 
             }
         }, {
             offset: "50%"
         });
-        $('#basketball').waypoint(function(direction) {
+        $('#nonTechnicalEvent3').waypoint(function(direction) {
             if( direction == "down") {
-                $('#basketball').addClass('mobileEffect');
-                $('#crossfit').removeClass('mobileEffect'); 
+                $('#nonTechnicalEvent3').addClass('mobileEffect');
+                $('#nonTechnicalEvent2').removeClass('mobileEffect'); 
             } else {
-                $('#basketball').removeClass('mobileEffect'); 
-                $('#crossfit').addClass('mobileEffect'); 
+                $('#nonTechnicalEvent3').removeClass('mobileEffect'); 
+                $('#nonTechnicalEvent2').addClass('mobileEffect'); 
             }
         }, {
             offset: "50%"
         });
-        $('#silambattam').waypoint(function(direction) {
+        $('#businessEvent1').waypoint(function(direction) {
             if( direction == "down") {
-                $('#silambattam').addClass('mobileEffect');
-                $('#basketball').removeClass('mobileEffect'); 
+                $('#businessEvent1').addClass('mobileEffect');
+                $('#nonTechnicalEvent3').removeClass('mobileEffect'); 
             } else {
-                $('#silambattam').removeClass('mobileEffect'); 
-                $('#basketball').addClass('mobileEffect'); 
+                $('#businessEvent1').removeClass('mobileEffect'); 
+                $('#nonTechnicalEvent3').addClass('mobileEffect'); 
             }
         }, {
             offset: "50%"
         });
-        $('#business-event-2').waypoint(function(direction) {
+        $('#businessEvent2').waypoint(function(direction) {
             if( direction == "down") {
-                $('#business-event-2').addClass('mobileEffect');
-                $('#silambattam').removeClass('mobileEffect'); 
+                $('#businessEvent2').addClass('mobileEffect');
+                $('#businessEvent1').removeClass('mobileEffect'); 
             } else {
-                $('#business-event-2').removeClass('mobileEffect'); 
-                $('#silambattam').addClass('mobileEffect'); 
+                $('#businessEvent2').removeClass('mobileEffect'); 
+                $('#businessEvent1').addClass('mobileEffect'); 
             }
         }, {
             offset: "50%"
@@ -93,9 +93,9 @@ $(document).ready(function() {
         
         $('#team-banner').waypoint(function(direction) {
             if( direction == "down") {
-                $('#business-event-2').removeClass('mobileEffect'); 
+                $('#businessEvent2').removeClass('mobileEffect'); 
             } else {
-                $('#business-event-2').addClass('mobileEffect'); 
+                $('#businessEvent2').addClass('mobileEffect'); 
             }
         }, {
             offset: "50%"
