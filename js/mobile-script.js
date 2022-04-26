e0267691069f22ff1f0a1cab1848d9b800aab719
AkashRaman <akashjayaraman8841@gmail.com>
2022-04-26 19:57:31 +0530

    Code Clear2

--- js/mobile-script.js
+++ js/mobile-script.js
@@ -105,7 +105,7 @@ $(document).ready(function() {
         
         $('#team-section').waypoint(function(direction) {
             if( direction == "down") {
-                $('#businessEvent2').removeClass('mobileEffect'); 
+                $('#businessEvent3').removeClass('mobileEffect'); 
             } else {
                 $('#businessEvent2').addClass('mobileEffect'); 
             }
@@ -113,111 +113,10 @@ $(document).ready(function() {
             offset: "50%"
         });
         
-        $('#teambox-1').waypoint(function(direction) {
-            if( direction == "down") {
-                $('#teambox-1').addClass('mobileEffect'); 
-            } else {
-                $('#teambox-1').removeClass('mobileEffect'); 
-            }
-        }, {
-            offset: "50%"
-        });
-        $('#teambox-2').waypoint(function(direction) {
-            if( direction == "down") {
-                $('#teambox-2').addClass('mobileEffect');
-                $('#teambox-1').removeClass('mobileEffect'); 
-            } else {
-                $('#teambox-2').removeClass('mobileEffect'); 
-                $('#teambox-1').addClass('mobileEffect'); 
-            }
-        }, {
-            offset: "50%"
-        });
-        $('#teambox-3').waypoint(function(direction) {
-            if( direction == "down") {
-                $('#teambox-3').addClass('mobileEffect');
-                $('#teambox-2').removeClass('mobileEffect'); 
-            } else {
-                $('#teambox-3').removeClass('mobileEffect'); 
-                $('#teambox-2').addClass('mobileEffect'); 
-            }
-        }, {
-            offset: "50%"
-        });
-        $('#teambox-4').waypoint(function(direction) {
-            if( direction == "down") {
-                $('#teambox-4').addClass('mobileEffect');
-                $('#teambox-3').removeClass('mobileEffect'); 
-            } else {
-                $('#teambox-4').removeClass('mobileEffect'); 
-                $('#teambox-3').addClass('mobileEffect'); 
-            }
-        }, {
-            offset: "50%"
-        });
-        $('#teambox-5').waypoint(function(direction) {
-            if( direction == "down") {
-                $('#teambox-5').addClass('mobileEffect');
-                $('#teambox-4').removeClass('mobileEffect'); 
-            } else {
-                $('#teambox-5').removeClass('mobileEffect'); 
-                $('#teambox-4').addClass('mobileEffect'); 
-            }
-        }, {
-            offset: "50%"
-        });
-        $('#teambox-6').waypoint(function(direction) {
-            if( direction == "down") {
-                $('#teambox-6').addClass('mobileEffect');
-                $('#teambox-5').removeClass('mobileEffect'); 
-            } else {
-                $('#teambox-6').removeClass('mobileEffect'); 
-                $('#teambox-5').addClass('mobileEffect'); 
-            }
-        }, {
-            offset: "50%"
-        });
-        $('#teambox-7').waypoint(function(direction) {
-            if( direction == "down") {
-                $('#teambox-7').addClass('mobileEffect');
-                $('#teambox-6').removeClass('mobileEffect'); 
-            } else {
-                $('#teambox-7').removeClass('mobileEffect'); 
-                $('#teambox-6').addClass('mobileEffect'); 
-            }
-        }, {
-            offset: "50%"
-        });
-        $('#teambox-8').waypoint(function(direction) {
-            if( direction == "down") {
-                $('#teambox-8').addClass('mobileEffect');
-                $('#teambox-7').removeClass('mobileEffect'); 
-            } else {
-                $('#teambox-8').removeClass('mobileEffect'); 
-                $('#teambox-7').addClass('mobileEffect'); 
-            }
-        }, {
-            offset: "50%"
-        });
-        
-        $('#classes-banner').waypoint(function(direction) {
-            if( direction == "down") {
-                $('#teambox-8').removeClass('mobileEffect'); 
-            } else {
-                $('#teambox-8').addClass('mobileEffect'); 
-            }
-        }, {
-            offset: "50%"
-        });
-        
         $('.class-item').removeClass('hover');
         $('.ftco-feature-1').removeClass('hover');
-        $('#about-right').removeClass('borderRight');
-        $('#about-left').removeClass('borderLeft');
     } else { 
         $('.class-item').addClass('hover');
         $('.ftco-feature-1').addClass('hover');
-        $('#about-right').addClass('borderRight');
-        $('#about-left').addClass('borderLeft');
     };
 });
